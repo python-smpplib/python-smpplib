8e57b3ea5a1e833af91d4397e6ba1d9b39b39c5a
yusufk <yusufk@294547dc-0e57-11de-a42d-873b86d837ff>
2009-03-11 16:50:47 +0000

    git-svn-id: http://smpplib.googlecode.com/svn/trunk/src@4 294547dc-0e57-11de-a42d-873b86d837ff

--- smpplib/command.py
+++ smpplib/command.py
@@ -16,7 +16,7 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #
-# Modified by Yusuf Kaka
+# Modified by Yusuf Kaka <yusufk at gmail>
 # Added support for Optional TLV's
 
 
--- smpplib/smpp.py
+++ smpplib/smpp.py
@@ -16,6 +16,9 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #
+#
+# Modified by Yusuf Kaka <yusufk at gmail>
+# Added support for Optional TLV's
 
 """SMPP module"""
 

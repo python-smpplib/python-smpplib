2eb8cd18391075f3313e20b3bf86f8f4ee4d22d1
jakob <jakob.oswald@gmx.net>
2014-07-17 00:17:37 +0200

    Adjusted simple sequence strategy to comply with spec

--- smpplib/client.py
+++ smpplib/client.py
@@ -35,15 +35,21 @@ logger = logging.getLogger('smpplib.client')
 
 class SimpleSequenceStrategy(object):
     
+    MIN_SEQUENCE = 0x00000001
+    MAX_SEQUENCE = 0x7FFFFFFF
+    
     def __init__(self):
-        self._sequence = 1
+        self._sequence = self.MIN_SEQUENCE
         
     @property
     def sequence(self):
         return self._sequence
     
     def next_sequence(self):
-        self._sequence += 1
+        if self._sequence == self.MAX_SEQUENCE:
+            self._sequence = self.MIN_SEQUENCE
+        else:
+            self._sequence += 1
         return self._sequence
 
 class Client(object):

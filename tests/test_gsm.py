924717625d09bc8d441b13d415a1e29b22f02e6d
Pavel Perestoronin <eigenein@gmail.com>
2018-10-15 16:44:26 +0300

    Fix UCS-2 encoding: fixes #49 and #53

--- tests/test_gsm.py
+++ tests/test_gsm.py
@@ -1,13 +1,13 @@
 # -*- coding: utf8 -*-
 
 import mock
-import pytest
+from pytest import mark, raises
 
 from smpplib import consts
 from smpplib.gsm import gsm_encode, make_parts, make_parts_encoded
 
 
-@pytest.mark.parametrize('plaintext, encoded_text', [
+@mark.parametrize('plaintext, encoded_text', [
     (u'@', b'\x00'),
     (u'^', b'\x1B\x14'),
 ])
@@ -15,15 +15,15 @@ def test_gsm_encode(plaintext, encoded_text):
     assert gsm_encode(plaintext) == encoded_text
 
 
-@pytest.mark.parametrize('plaintext', [
+@mark.parametrize('plaintext', [
     (u'Ая',),
 ])
 def test_gsm_encode_unicode_error(plaintext):
-    with pytest.raises(UnicodeError):
+    with raises(UnicodeError):
         gsm_encode(plaintext)
 
 
-@pytest.mark.parametrize('plaintext, encoding, expected_parts, expected_encoding', [
+@mark.parametrize('plaintext, encoding, expected_parts, expected_encoding', [
     (u'@', consts.SMPP_ENCODING_DEFAULT, [b'\x00'], consts.SMPP_ENCODING_DEFAULT),
     (u'Ая', consts.SMPP_ENCODING_DEFAULT, [b'\x04\x10\x04O'], consts.SMPP_ENCODING_ISO10646),
     (u'é', consts.SMPP_ENCODING_ISO88591, [b'\xe9'], consts.SMPP_ENCODING_ISO88591),
@@ -32,10 +32,10 @@ def test_make_parts_single(plaintext, encoding, expected_parts, expected_encodin
     assert make_parts(plaintext, encoding) == (expected_parts, expected_encoding, consts.SMPP_MSGTYPE_DEFAULT)
 
 
-@pytest.mark.parametrize('plaintext, expected', [
-    (u'@' * consts.SEVENBIT_MP_SIZE * 2, [
-        b'\x05\x00\x03\x42\x02\x01' + b'\x00' * consts.SEVENBIT_MP_SIZE,
-        b'\x05\x00\x03\x42\x02\x02' + b'\x00' * consts.SEVENBIT_MP_SIZE,
+@mark.parametrize('plaintext, expected', [
+    (u'@' * consts.SEVENBIT_PART_SIZE * 2, [
+        b'\x05\x00\x03\x42\x02\x01' + b'\x00' * consts.SEVENBIT_PART_SIZE,
+        b'\x05\x00\x03\x42\x02\x02' + b'\x00' * consts.SEVENBIT_PART_SIZE,
     ]),
 ])
 def test_make_parts_multiple(plaintext, expected):
@@ -44,7 +44,7 @@ def test_make_parts_multiple(plaintext, expected):
         assert make_parts(plaintext) == (expected, consts.SMPP_ENCODING_DEFAULT, consts.SMPP_GSMFEAT_UDHI)
 
 
-@pytest.mark.parametrize('encoded_text, part_size, expected', [
+@mark.parametrize('encoded_text, part_size, expected', [
     (b'12345', 5, [b'\x05\x00\x03\x42\x01\x0112345']),
     (b'12345', 2, [b'\x05\x00\x03\x42\x03\x0112', b'\x05\x00\x03\x42\x03\x0234', b'\x05\x00\x03\x42\x03\x035']),
 ])
@@ -52,3 +52,11 @@ def test_make_parts_encoded(encoded_text, part_size, expected):
     with mock.patch('random.randint') as randint:
         randint.return_value = 0x42
         assert make_parts_encoded(encoded_text, part_size) == expected
+
+
+@mark.parametrize('text, expected', [
+    (u'Привет мир!\n' * 10, 2),
+])
+def test_part_number(text, expected):
+    parts, _, _ = make_parts(text)
+    assert len(parts) == expected

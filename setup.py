130f5d04d08a1f00671d89812874c42cf65eb740
Pavel Perestoronin <eigenein@gmail.com>
2018-11-27 13:23:11 +0100

    Remove 2.6 from setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,6 @@ setup(
         'License :: OSI Approved',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',

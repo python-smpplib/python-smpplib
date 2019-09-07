babb0eb7ee32aaa3ea295eecf3867b49638efe62
Lynesth <lyn@nautile.nc>
2019-09-07 19:14:05 +1100

    Add timeout to Client's properties

--- smpplib/client.py
+++ smpplib/client.py
@@ -68,7 +68,7 @@ class Client(object):
         self.host = host
         self.port = int(port)
         self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self._socket.settimeout(timeout)
+        self.timeout = timeout
         if sequence_generator is None:
             sequence_generator = SimpleSequenceGenerator()
         self.sequence_generator = sequence_generator
@@ -106,6 +106,7 @@ class Client(object):
         try:
             if self._socket is None:
                 self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                self._socket.settimeout(self.timeout)
             self._socket.connect((self.host, self.port))
             self.state = consts.SMPP_CLIENT_STATE_OPEN
         except socket.error:
